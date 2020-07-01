9401fdddb398298859ed506cd5e2ce0ca165b762
SotskovS <svalgele@gmail.com>
2020-07-01 11:47:38 +0300

    add subscription, comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :events
+  has_many :comments
+  has_many :subscriptions
 
   validates :name, presence: true, length: {maximum: 35}
 
