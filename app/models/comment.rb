562276a1fd5e616cdc50f7b6c673506550ec27aa
SotskovS <svalgele@gmail.com>
2020-07-09 07:04:26 +0300

    add mailgun

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :event
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :event, presence: true
   validates :body, presence: true
@@ -9,7 +9,7 @@ class Comment < ApplicationRecord
 
   def user_name
   	if user.present?
-  		user.user_name
+  		user.name
   	else
   		super
   	end
